3954968641aa4a5648a48fb1550bb3e5f55eadc9
wfrost96 <53651633+wfrost96@users.noreply.github.com>
2020-05-08 19:00:58 +0100

    Update search.php

--- peers/search.php
+++ peers/search.php
@@ -9,6 +9,11 @@ if(isset($_POST['SubmitButton'])){
   <head>
   </head>
   <body>
+
+    <h1>Peers' interests</h1>
+
+    <h2>Search for peers based on interests. Type something in the box to get started.</h2>
+
     <form action="" method="post">
       <input list="interests" name="inputText">
       <datalist id="interests">
@@ -30,6 +35,10 @@ if(isset($_POST['SubmitButton'])){
     #echo $interest_searched;
 
     ?>
+
+    <br><br><br><br>
+
+    <p>Code available <a href="https://github.com/wfrost96/parliwho_public">here</a>. Copyright williamfro.st 2020.</p>
 
   </body>
 </html>
