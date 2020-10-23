b4f61b853525313a56ee4f53db72245b17ff0da6
wfrost96 <53651633+wfrost96@users.noreply.github.com>
2020-10-23 18:51:16 +0100

    significant update
    Functions added to make code easier to read.
    
    Gov/opposition distinction in posts removed; now just post, frontbench, payroll, cabinet for simplicity.

--- mps/scrape.py
+++ mps/scrape.py
@@ -21,71 +21,73 @@ for i in range(1, 34):
         del link_member_letters[-7:]
         link_member_full = "".join(link_member_letters) + "career"
         list_of_links.append(link_member_full)
-#print(list_of_links)"""
+#print(list_of_links)
 
 mps_list = []
 
-for mp_link in list_of_links:
-    #print(mp_link)
-    webpage = requests.get(mp_link)
-    soup = BeautifulSoup(webpage.content, "html.parser")
-
-    mp = []
-
-    #get name
+def get_name(soup):
     name_raw = soup.find("h1")
     name_titles = name_raw.get_text()
-    name_letters = []
+
     for letter in name_titles:
         name_letters.append(letter)
+
     #remove MP post-nominal
     del name_letters[-3:]
-    #print(name_letters)
-
     #check for title: Rt Hon
     if name_letters[0] == "R" and name_letters[1] == "t":
         del name_letters[:7]
-    #print(name_letters)
-
     #check for title: Dr
     if name_letters[0] == "D" and name_letters[1] == "r" and name_letters[2] == " ":
         del name_letters[:3]
-    #print(name_letters)
-
     #check for title: Sir
     if name_letters[0] == "S" and name_letters[1] == "i" and name_letters[2] == "r" and name_letters[3] == " ":
         del name_letters[:4]
-    #print(name_letters)
-
+    #check for title: Mr
+    if name_letters[0] == "M" and name_letters[1] == "r" and name_letters[2] == " ":
+        del name_letters[:3]
     #check for title: Ms
     if name_letters[0] == "M" and name_letters[1] == "s" and name_letters[2] == " ":
         del name_letters[:3]
-    #print(name_letters)
 
     #return list as string
     name = "".join(name_letters)
-    #print(name)
-    mp.append(name)
-    #print(mp)
+    return(name)
 
-    #link to image file (which has to be manually downloaded, stored, and renamed appropriately)
+def get_picture_link(name):
     link_beginning = "<img src=\"all_mps/"
     link_end = ".png\">"
     name_link = []
-    for letter in name_letters:
+    for letter in name:
         if letter != " ":
             name_link.append(letter.lower())
         else:
             name_link.append("_")
     picture_link = ""
     picture_link = link_beginning + picture_link.join(name_link) + link_end
-    #print(picture_link)
-    mp.append(picture_link)
-    #print(mp)
+    return picture_link
 
-    #get constituency
+import gender_guesser.detector
+def get_mp_gender(name):
+    firstname = name.split()[0]
+    d = gender_guesser.detector.Detector()
+    gender = d.get_gender(firstname)
+
+    if gender == "male":
+        gender = "m"
+    elif gender == "mostly_male":
+        gender = "m"
+    elif gender == "female":
+        gender = "f"
+    elif gender == "mostly_female":
+        gender = "f"
+    else:
+        gender = "unknown"
+
+    return gender
+
+def get_constituency(soup):
     constituency_tag = soup.select("a[href*=constituency] > div[class=card-inner] > div[class=content]> div[class=primary-info]")
-    #print(constituency)
     constituency = ""
     for i in constituency_tag:
         constituency_raw = i.text
@@ -95,12 +97,10 @@ for mp_link in list_of_links:
         del constituency_char[:18]
         del constituency_char[-14:]
         constituency = "".join(constituency_char)
-    #print(constituency)
-    mp.append(constituency)
+    return constituency
 
-    #get party
+def get_party(soup):
     party_tag = soup.select("a[href*=partyid] > div[class=card-inner] > div[class=content]> div[class=primary-info]")
-    #print(party_tag)
     party = ""
     for i in party_tag:
         party_raw = i.text
@@ -110,12 +110,10 @@ for mp_link in list_of_links:
         del party_char[:18]
         del party_char[-14:]
         party = "".join(party_char)
-    #print(party)
-    mp.append(party)
+    return party
 
-    #get intake year
+def get_intake(soup):
     intake_tag = soup.select("a[href*=constituency] > div[class=card-inner] > div[class=info] > div[class=info-inner] > div[class=indicators-left]")
-    #print(intake_tag)
     intake = ""
     for i in intake_tag:
         intake_raw = i.text
@@ -124,86 +122,108 @@ for mp_link in list_of_links:
             intake_char.append(letter)
         del intake_char[-67:]
         intake = "".join(intake_char[-4:])
-    #print(intake)
-    mp.append(intake)
-    #print(mp)
+    return(intake)
 
-    #
-    #Gov/Opposition posts are coded inconsistently. To find them, we see first need to find the h4 tags, and then execute the relevant code for each h4 tag, if found.
-    #
-
-    #find h4 tags
+def get_post_list(soup):
     post_tags = soup.select("h4")
     post_list = []
     for item in post_tags:
         for i in item:
             post_list.append(i)
-    #print(post_list)
+    return post_list
 
-    #execute the relevant code for each h4 tag, if found
+def get_post(soup):
     if "Government post" in post_list:
-        #print("Has or has had Government post")
         gov_post_list = soup.find_all('div', class_='primary-info')
         gov_post_raw = gov_post_list[2]
         for item in gov_post_raw:
             gov_post = item.strip()
-        #print(gov_post)
-        mp.append(gov_post)
+        return gov_post
 
-        gov_post_dep_list = soup.find_all('div', class_='secondary-info')
-        gov_post_dep_raw = gov_post_dep_list[1]
-        for item in gov_post_dep_raw:
-            gov_post_dep = item.strip()
-        #print(gov_post_dep)
-        mp.append(gov_post_dep)
-    else:
-        mp.append("no")
-        mp.append("no")
-
-
-    if "Opposition post" in post_list:
-        print("Has or has had Opposition post")
-
-        #get the opposition post
+    elif "Opposition post" in post_list:
         #sometimes class=card is a div and sometimes it is an a, so account for both scenarios to get the data we want
         opposition_post_tag = soup.select("div[class=card-list] > div[class=card] > div[class=card-inner] >  div[class=content] > div[class=primary-info]")
         if opposition_post_tag:
             for item in opposition_post_tag:
                 for i in item:
                     opposition_post = i.strip()
-                    mp.append(opposition_post)
+                    return opposition_post
         else:
-            #print("list is empty")
             opposition_post_tag = soup.select("div[class=card-list] > a[class=card] > div[class=card-inner] >  div[class=content] > div[class=primary-info]")
             for item in opposition_post_tag:
                 for i in item:
                     if i.strip() != constituency:
                         opposition_post = i.strip()
-                        mp.append(opposition_post)
+                        return opposition_post
 
-        #get the opposition post department
+    else:
+        post = "no"
+        return post
+
+def get_frontbench(post):
+    if post != "no":
+        frontbench = "yes"
+    else:
+        frontbench = "no"
+    return frontbench
+
+def get_payroll(post):
+    if post != "no":
+        payroll = "yes"
+    else:
+        payroll = "no"
+    return payroll
+
+#cabinet
+webpage = requests.get("https://members.parliament.uk/Government/Cabinet")
+soup = BeautifulSoup(webpage.content, "html.parser")
+
+cabinet = soup.find_all('div', class_='primary-info')
+
+cabinet_list = []
+for mp in cabinet:
+    cabinet_list.append(mp.get_text().strip())
+#print(cabinet_list)
+
+def get_cabinet(name):
+    if name in cabinet_list:
+        cabinet = "yes"
+    else:
+        cabinet = "no"
+    return cabinet
+
+def get_post_dep(soup):
+    if "Government post" in post_list:
+        gov_post_dep_list = soup.find_all('div', class_='secondary-info')
+        gov_post_dep_raw = gov_post_dep_list[1]
+        for item in gov_post_dep_raw:
+            gov_post_dep = item.strip()
+        #print(gov_post_dep)
+        #mp.append(gov_post_dep)
+        return gov_post_dep
+    elif "Opposition post" in post_list:
         #sometimes class=card is a div and sometimes it is an a, so account for both scenarios to get the data we want
         opposition_post_dep_tag = soup.select("div[class=card-list] > div[class=card] > div[class=card-inner] >  div[class=content] > div[class=secondary-info]")
         if opposition_post_dep_tag:
             for item in opposition_post_dep_tag:
                 for i in item:
                     opposition_post_dep = i.strip()
-                    print(opposition_post_dep)
-                    mp.append(opposition_post_dep)
+                    return opposition_post_dep
         else:
-            print("list is empty")
             opposition_post_dep_tag = soup.select("div[class=card-list] > a[class=card] > div[class=card-inner] >  div[class=content] > div[class=secondary-info]")
             for item in opposition_post_dep_tag:
                 for i in item:
                     if "Elected" not in i.strip():
-                        opposition_post = i.strip()
-                        mp.append(opposition_post)
-    else:
-        mp.append("no")
-        mp.append("no")
+                        opposition_post_dep = i.strip()
+                        return opposition_post_dep
 
+    else:
+        post_dep = "no"
+        return post_dep
+
+from commons_committee_list import *
+def get_select_committees(soup):
     if "Committee memberships" in post_list:
-        #print("Has or has had Committee memberships")
 
         #get all references to committees past and present on the member's profile
         committee_tag = soup.select("[href*=committee]")
@@ -214,10 +234,8 @@ for mp_link in list_of_links:
                 str_i = str(i)
                 if "Present" in str_i:
                     committee_tag_list.append(str_i)
-        #print(committee_tag_list)
-
-        #import commons committee list from separate file
-        from commons_committee_list import *
+                #else: #MP doesn't currently sit on this select committee
+                    #return "no"
 
         #for each committee, see if the MP currently sits on it, and if so record the committee
         select_committees = []
@@ -227,16 +245,73 @@ for mp_link in list_of_links:
         if select_committees:
             if len(select_committees) == 1:
                 for item in select_committees:
-                    mp.append(item)
+                    return item
             elif len(select_committees) > 1:
                 for item in select_committees:
                     select_committees_str = "; ".join(select_committees)
-                    mp.append(select_committees_str)
-        else:
-            mp.append("no")
-    else:
+                    return select_committees_str
+    else: #member isn't on any select committees
+        return "no"
+
+for mp_link in list_of_links:
+    #print(mp_link)
+    webpage = requests.get(mp_link)
+    soup = BeautifulSoup(webpage.content, "html.parser")
+
+    mp = []
+
+    #get name
+    name_letters = []
+    name = get_name(soup)
+    mp.append(name)
+
+    #get picture link
+    picture_link = get_picture_link(name)
+    mp.append(picture_link)
+
+    #get gender
+    gender = get_mp_gender(name)
+    mp.append(gender)
+
+    #get constituency
+    constituency = get_constituency(soup)
+    mp.append(constituency)
+
+    #get party
+    party = get_party(soup)
+    mp.append(party)
+
+    #get intake year
+    intake = get_intake(soup)
+    mp.append(intake)
+
+    #get gov/opposition posts
+    post_list = get_post_list(soup)
+
+    post = get_post(soup)
+    mp.append(post)
+
+    frontbench = get_frontbench(post)
+    mp.append(frontbench)
+
+    payroll = get_payroll(post)
+    mp.append(payroll)
+
+    cabinet = get_cabinet(name)
+    mp.append(cabinet)
+
+    #get gov/opposition posts dep
+    post = get_post_dep(soup)
+    mp.append(post)
+
+    #get select committees
+    select_committees = get_select_committees(soup)
+    if select_committees is None: #this covers cases where an MP used to sit on a select committee but no longer does. can this be got into the function?
         mp.append("no")
-        
+    else:
+        mp.append(select_committees)
+
+    #add the mp to the big list
     mps_list.append(mp)
 
-print(mps_list)
+#print(mps_list)
